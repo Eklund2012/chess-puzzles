05b360f1623314adccd4aacd3506db72e4eaff03
Eklund2012 <david.eklund9@gmail.com>
2025-04-03 15:03:11 +0200

    updated tests

--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-import chess_game
-from main import get_square_from_pos  # Example function
-
-def test_get_square_from_pos():
-    square = get_square_from_pos((100, 100))  # Assuming a standard 8x8 board
-    assert isinstance(square, chess_game.Square)  # Check it returns a valid chess square
